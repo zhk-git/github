683ff9169abee5bdb8c18412d40a737b242a7c11
zhk-git <58315229+zhk-git@users.noreply.github.com>
2019-11-29 19:48:40 +0800

    Update and rename src/main/java/com/baizhi/github/GithubApplication.java to  src/ main/ java/ com/ baizhi/ github/GithubApplication.java

--- src/ main/ java/ com/ baizhi/ github/GithubApplication.java
+++ src/ main/ java/ com/ baizhi/ github/GithubApplication.java
@@ -9,6 +9,7 @@ public class GithubApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubApplication.class, args);
         System.out.println();
+        System.out.println("123");
     }
 
 }
